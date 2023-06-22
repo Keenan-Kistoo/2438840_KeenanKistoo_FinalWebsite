1c92c810a3030e3de501a796e2ef3e32774e9bc0
KeenanKistoo <2438840@students.wits.ac.za>
2023-06-22 17:52:45 +0200

    Nav Bar JS Test

---
+++ app.js
@@ -0,0 +1,25 @@
+//Navigation Bar Code:
+
+let navBar = document.querySelector(".nav-bar");
+
+function SetNavBar(count) {
+  const { title, classOne, classTwo, classThree, classFour } = details;
+  navBar.innerHTML = `<section class="heading">
+  <h1 class="main-head">${details[count].title}</h1>
+  </section>
+  <section class="tabs">
+  <!--Container-->
+  <ul class="links">
+    <!--Indvidual Links-->
+    <li>
+      <a class="${details[count].classHome}" href="index.html">Homepage</a>
+    </li>
+    <li><a class="${details[count].classBlog}" href="blog.html">Blogs</a></li>
+    <li><a class="${details[count].classDesign}" href="design.html">Designs</a></li>
+    <li>
+      <a class="${details[count].classPort}" href="portfolio.html">Portfolio</a>
+    </li>
+    <li><a class="${details[count].classEssay}" href="essays.html">Essays</a></li>
+  </ul>
+  </section>`;
+}
---
+++ data.js
@@ -0,0 +1,43 @@
+//Nav Bar Information:
+const details = [
+  {
+    title: "Blog Section",
+    classHome: "home-links",
+    classBlog: "home-links active",
+    classDesign: "home-links",
+    classPort: "home-links",
+    classEssay: "home-links",
+  },
+  {
+    title: "Design Section",
+    classHome: "home-links",
+    classBlog: "home-links",
+    classDesign: "home-links active",
+    classPort: "home-links",
+    classEssay: "home-links",
+  },
+  {
+    title: "Portfolio Section",
+    classHome: "home-links",
+    classBlog: "home-links",
+    classDesign: "home-links",
+    classPort: "home-links active",
+    classEssay: "home-links",
+  },
+  {
+    title: "Essay Section",
+    classHome: "home-links",
+    classBlog: "home-links",
+    classDesign: "home-links",
+    classPort: "home-links",
+    classEssay: "home-links active",
+  },
+  {
+    title: "Blog Posts",
+    classHome: "home-links",
+    classBlog: "home-links",
+    classDesign: "home-links",
+    classPort: "home-links",
+    classEssay: "home-links ",
+  },
+];
