3387123d0d2d9785af791ca14a97720944eb8734
KeenanKistoo <2438840@students.wits.ac.za>
2023-06-22 20:26:29 +0200

    Filter Complete For Blog Posts

--- app.js
+++ app.js
@@ -47,3 +47,50 @@ const GenBlogPosts = () => {
   ready = true;
   //console.log(ready);
 };
+
+let allButton = document.getElementById("all-button");
+
+allButton.addEventListener("click", function (event) {
+  GenBlogPosts();
+});
+
+const blogSect = document.querySelector(".blog");
+const genBlogSemOne = () => {
+  const filteredPosts = blogInfo.filter((post) => post.id >= 0 && post.id < 6);
+
+  blogSect.innerHTML = filteredPosts
+    .map((post) => {
+      const { icon, link, heading, desc, date, dataCode } = post;
+      return `<article class="blog-post">
+    <img src="${icon}" alt="" class="blog-img" />
+    <a data-week="${dataCode}" href="${link}" class="blog-head">${heading}</a>
+    <p class="blog-desc">${desc}</p>
+    <p class="blog-date">${date}</p>
+  </article>`;
+    })
+    .join("");
+};
+
+let semOne = document.getElementById("first-button");
+
+semOne.addEventListener("click", genBlogSemOne);
+
+const genBlogSemTwo = () => {
+  const filteredPosts = blogInfo.filter((post) => post.id >= 6 && post.id < 12);
+
+  blogSect.innerHTML = filteredPosts
+    .map((post) => {
+      const { icon, link, heading, desc, date, dataCode } = post;
+      return `<article class="blog-post">
+    <img src="${icon}" alt="" class="blog-img" />
+    <a data-week="${dataCode}" href="${link}" class="blog-head">${heading}</a>
+    <p class="blog-desc">${desc}</p>
+    <p class="blog-date">${date}</p>
+  </article>`;
+    })
+    .join("");
+};
+
+let semTwo = document.getElementById("second-button");
+
+semTwo.addEventListener("click", genBlogSemTwo);
